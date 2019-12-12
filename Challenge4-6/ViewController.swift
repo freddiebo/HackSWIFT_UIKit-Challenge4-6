ba7594749376fb4c435459f3244b8dc2c8547b22
VBondarev <freddiebo@yandex.ru>
2019-12-12 15:45:49 +0300

    Initial Commit

---
+++ Challenge4-6/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Challenge4-6
+//
+//  Created by  Vladislav Bondarev on 12.12.2019.
+//  Copyright © 2019 Vladislav Bondarev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
